f562f0ac368ffe2a57c96c4f0d9e6f31f19fd320
whoismissing <whereismissing@gmail.com>
2020-02-23 13:40:11 -0600

    Fixed bug where mobile layout unstickied is behind the content

--- assets/scripts/mobile_nav_bar.js
+++ assets/scripts/mobile_nav_bar.js
@@ -3,7 +3,7 @@
 function access_mobile_nav_bar() {
     var x = document.getElementById("navbar");
     if (x.className === "topnav" ) {
-        x.className += " responsive";
+        x.className += " responsive sticky";
     } else if (x.className === "topnav sticky") {
         x.className += " responsive";
     } else { // reset back to topnav
@@ -19,4 +19,32 @@ function show_mobile_element(var_id) {
     } else {
         x.style.display = "none";
     }
+}
+
+function contains(a, obj) {
+    var i = a.length;
+    while (i--) {
+       if (a[i] === obj) {
+           return true;
+       }
+    }
+    return false;
+}
+
+/* 
+ * Fix for bug where mobile layout unstickied is behind the content,
+ * attempted modifying z-index to no avail with css. So instead, our
+ * hack is to do nothing if the navbar is "topnav responsive sticky"
+*/
+function list_includes_topnav_responsive_sticky(navbarlist) {
+    if (contains(navbarlist, "topnav") != true) {
+	return false;
+    }
+    if (contains(navbarlist, "responsive") != true) {
+	return false;
+    }
+    if (contains(navbarlist, "sticky") != true) {
+	return false;
+    }
+    return true;
 }
